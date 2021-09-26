a465316ade3c4e60bb5961f2dda4558c19ed3abf
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-09-26 23:06:08 +0100

    Add the users to the Users table

--- models.py
+++ models.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('sqlite:///users.db', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
 Base = declarative_base()
 
 class User(Base):
@@ -18,3 +21,22 @@ class User(Base):
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
 
+    # rhe_user = User(name='Rheanne', fullname='Rheanne McIntosh', nickname='Rhe')
+    # print(rhe_user.name)
+    # print(rhe_user.id)
+
+    # session.add(rhe_user)
+    # session.commit()
+    # print(rhe_user.id)
+
+    new_users = [
+        User(name='Grace', fullname='Grace Hopper', nickname='Pioneer'),
+        User(name='Alan', fullname='Alan Turing', nickname='Computer Scientist'),
+        User(name='Katherine', fullname='Katherine Johnson', nickname='')
+    ]
+
+    session.add_all(new_users)
+    session.commit()
+
+    for user in new_users:
+        print(user.id)
